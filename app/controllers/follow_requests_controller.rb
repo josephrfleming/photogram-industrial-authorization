d8192351cc39e47da7f8202b32714ed7348e2a43
Joseph Fleming <162403379+josephrfleming@users.noreply.github.com>
2025-03-21 00:51:47 +0000

    updates

--- app/controllers/follow_requests_controller.rb
+++ app/controllers/follow_requests_controller.rb
@@ -1,19 +1,13 @@
 class FollowRequestsController < ApplicationController
   before_action :set_follow_request, only: %i[ show edit update destroy ]
-  before_action :ensure_authorized, only: [:update, :destroy]
 
-  # GET /follow_requests
+  # GET /follow_requests or /follow_requests.json
   def index
-    # Show only pending follow requests where the current user is the recipient.
-    @follow_requests = FollowRequest.where(recipient: current_user)
+    @follow_requests = FollowRequest.all
   end
 
-  # GET /follow_requests/1
+  # GET /follow_requests/1 or /follow_requests/1.json
   def show
-    # Optionally, restrict viewing to the sender or recipient.
-    unless current_user == @follow_request.sender || current_user == @follow_request.recipient
-      redirect_back fallback_location: root_url, alert: "Not authorized."
-    end
   end
 
   # GET /follow_requests/new
@@ -23,13 +17,9 @@ class FollowRequestsController < ApplicationController
 
   # GET /follow_requests/1/edit
   def edit
-    # Only the sender should be able to edit a follow request (for example, to cancel it).
-    unless current_user == @follow_request.sender
-      redirect_back fallback_location: root_url, alert: "Not authorized."
-    end
   end
 
-  # POST /follow_requests
+  # POST /follow_requests or /follow_requests.json
   def create
     @follow_request = FollowRequest.new(follow_request_params)
     @follow_request.sender = current_user
@@ -45,7 +35,7 @@ class FollowRequestsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /follow_requests/1
+  # PATCH/PUT /follow_requests/1 or /follow_requests/1.json
   def update
     respond_to do |format|
       if @follow_request.update(follow_request_params)
@@ -58,7 +48,7 @@ class FollowRequestsController < ApplicationController
     end
   end
 
-  # DELETE /follow_requests/1
+  # DELETE /follow_requests/1 or /follow_requests/1.json
   def destroy
     @follow_request.destroy
     respond_to do |format|
@@ -68,29 +58,13 @@ class FollowRequestsController < ApplicationController
   end
 
   private
-
+    # Use callbacks to share common setup or constraints between actions.
     def set_follow_request
       @follow_request = FollowRequest.find(params[:id])
     end
 
-    # Only allow trusted parameters.
-    # Note: We remove :sender_id since the sender is always current_user.
+    # Only allow a list of trusted parameters through.
     def follow_request_params
-      params.require(:follow_request).permit(:recipient_id, :status)
-    end
-
-    # Ensure that only authorized users can update or destroy a follow request.
-    # For update (accept/reject), only the recipient is allowed.
-    # For destroy, either the sender (cancelling) or the recipient (rejecting) is allowed.
-    def ensure_authorized
-      if action_name == "update"
-        unless current_user == @follow_request.recipient
-          redirect_back fallback_location: root_url, alert: "Not authorized."
-        end
-      elsif action_name == "destroy"
-        unless current_user == @follow_request.sender || current_user == @follow_request.recipient
-          redirect_back fallback_location: root_url, alert: "Not authorized."
-        end
-      end
+      params.require(:follow_request).permit(:recipient_id, :sender_id, :status)
     end
 end
